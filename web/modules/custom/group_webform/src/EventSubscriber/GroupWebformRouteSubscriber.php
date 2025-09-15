c8bd754cd779614e7911f877912e9315ce62bedc
Paul Smith <paul.smith@manifesto.co.uk>
2025-09-15 11:35:54 +0100

    feat (WA-393): Tidy up access to Group paths, and remove duplicated Group Roles.

---
+++ web/modules/custom/group_webform/src/EventSubscriber/GroupWebformRouteSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\group_webform\EventSubscriber;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Route subscriber.
+ */
+final class GroupWebformRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection): void {
+
+    // Hide unneeded tabs.
+    foreach ([
+      'entity.group_relationship.collection',
+      'entity.group.webform',
+      'entity.group_content_menu.add_menu_link',
+      'entity.group_content_menu.collection',
+    ] as $route_name) {
+      if ($route = $collection->get($route_name)) {
+        $route->setRequirement('_access', 'FALSE');
+      }
+    }
+
+    // Ensure routes use the admin theme.
+    foreach ([
+      'entity.group.version_history',
+      'view.group_members.page_1',
+    ] as $route_name) {
+      if ($route = $collection->get($route_name)) {
+        $route->setOption('_admin_route', TRUE);
+      }
+    }
+  }
+
+}
