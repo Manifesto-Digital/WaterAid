4e00205308010ed27b42e64d476135b059cc99db
Paul Smith <paul.smith@manifesto.co.uk>
2025-09-17 15:28:27 +0100

    feat (WA-390): Remove deprecated module, add required custom module, change format in webforms, and remove jQuery date-pickers..

---
+++ web/modules/custom/webform_capture_plus/src/WebformAddressCapturePlusConstants.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\webform_capture_plus;
+
+/**
+ * Webform Address CapturePlus Constants.
+ *
+ * @package Drupal\webform_capture_plus
+ */
+abstract class WebformAddressCapturePlusConstants {
+
+  /**
+   * Machine name for the field that captures the setting's state.
+   */
+  public const WEBFORM_CAPTURE_PLUS_ACTIVE = 'capture_plus_active';
+
+  /**
+   * Machine name for the field that captures the label name.
+   */
+  public const WEBFORM_CAPTURE_LOOKUP_LABEL = 'capture_plus_lookup_label';
+
+  /**
+   * Machine name for the field that captures the placeholder.
+   */
+  public const WEBFORM_CAPTURE_PLACEHOLDER = 'capture_plus_placeholder';
+
+}
