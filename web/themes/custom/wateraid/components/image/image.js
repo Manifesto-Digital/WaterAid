5911683d7be3f4bd47eda9088ac7768e49c012e6
Lisa Webster <lisa.webster@manifesto.co.uk>
2025-09-22 12:09:52 +0100

    feat: [WA-324] Linting

--- web/themes/custom/wateraid/components/image/image.js
+++ web/themes/custom/wateraid/components/image/image.js
@@ -1,9 +1,9 @@
 (function (Drupal) {
   Drupal.behaviors.image = {
     attach(context) {
-      let openButton = context.getElementById('image-caption-open');
-      let closeButton = context.getElementById('image-caption-close');
-      let image = context.querySelector('.image__wrapper');
+      let openButton = context.getElementById("image-caption-open");
+      let closeButton = context.getElementById("image-caption-close");
+      let image = context.querySelector(".image__wrapper");
 
       console.log(openButton);
       console.log(closeButton);
