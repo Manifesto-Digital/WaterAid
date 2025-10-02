05cff155b39f049e8f343f0f273b0e28b46ca1b9
Paul Smith <paul.smith@manifesto.co.uk>
2025-10-02 15:50:36 +0100

    feat (WA-253): Holding commit creating a new field type that will eventually implement the DAM link.

---
+++ docroot/modules/custom/wa_orange_dam/src/Plugin/Field/FieldType/ImageItem.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\wa_orange_dam\Plugin\Field\FieldType;
+
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Defines the 'wa_orange_dam_image' field type.
+ *
+ * @FieldType(
+ *   id = "wa_orange_dam_image",
+ *   label = @Translation("Orange DAM Image"),
+ *   description = @Translation("Some description."),
+ *   default_widget = "string_textfield",
+ *   default_formatter = "string",
+ * )
+ */
+final class ImageItem extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty(): bool {
+    return match ($this->get('value')->getValue()) {
+      NULL, '' => TRUE,
+      default => FALSE,
+    };
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition): array {
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(t('Text value'))
+      ->setRequired(TRUE);
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConstraints(): array {
+    $constraints = parent::getConstraints();
+
+    $constraint_manager = $this->getTypedDataManager()->getValidationConstraintManager();
+
+    // @DCG Suppose our value must not be longer than 10 characters.
+    $options['value']['Length']['max'] = 10;
+
+    // @DCG
+    // See /core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint
+    // directory for available constraints.
+    $constraints[] = $constraint_manager->create('ComplexData', $options);
+    return $constraints;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition): array {
+
+    $columns = [
+      'value' => [
+        'type' => 'varchar',
+        'not null' => FALSE,
+        'description' => 'Column description.',
+        'length' => 255,
+      ],
+    ];
+
+    $schema = [
+      'columns' => $columns,
+      // @todo Add indexes here if necessary.
+    ];
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function generateSampleValue(FieldDefinitionInterface $field_definition): array {
+    $random = new Random();
+    $values['value'] = $random->word(mt_rand(1, 50));
+    return $values;
+  }
+
+}
