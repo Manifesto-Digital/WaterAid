f28c5d8c09fb16358a36532ac2075cbf47e93867
Paul Smith <paul.smith@manifesto.co.uk>
2025-11-03 14:56:14 +0000

    feat (WA-419): Add image paragraph migration, and delete unused media types to prevent confusion going forward.

--- docroot/modules/custom/wa_migration/src/Plugin/migrate/process/AssetToOrangeDam.php
+++ docroot/modules/custom/wa_migration/src/Plugin/migrate/process/AssetToOrangeDam.php
@@ -59,30 +59,30 @@ final class AssetToOrangeDam extends ProcessPluginBase implements ContainerFacto
     if ($value) {
       $return = [];
 
-      if ($result = $this->api->search(['query' => 'image-id-(asset-bank).Image-ID-(Asset-Bank):' . $value])) {
-        if (!isset($result['APIResponse']['Items'][0]['SystemIdentifier'])) {
+      // For speed, we'll assume all images have had their asset bank id set.
+      if ($destination_property == 'field_media_dam_image') {
+        $result = $this->api->search(['query' => 'image-id-(asset-bank).Image-ID-(Asset-Bank):' . $value]);
+      }
+      else {
 
-          // If we don't get it by the asset bank id, we'll try searching for
-          // the value in the filename.
-          $result = $this->api->search(['query' => 'CoreField.OriginalFileName:' . $value]);
+        // For the files, we'll search only by file name.
+        $result = $this->api->search(['query' => 'CoreField.OriginalFileName:' . $value]);
+      }
 
-          if (!empty($result['APIResponse']['Items']) && count($result['APIResponse']['Items']) > 1) {
+      if (!empty($result['APIResponse']['Items']) && count($result['APIResponse']['Items']) > 1) {
 
-            // We have no way of deduping the wildcard searches, so if we have
-            // more than one result, we'll have to skip them.
-            return NULL;
-          }
-        }
+        // We have no way of deduping the wildcard searches, so if we have
+        // more than one result, we'll have to skip them.
+        return NULL;
+      }
 
-        // If there is more than one asset with the same id, we're just going to use the first.
-        if (isset($result['APIResponse']['Items'][0]['SystemIdentifier'])) {
-          $return['system_identifier'] = $result['APIResponse']['Items'][0]['SystemIdentifier'];
+      if (isset($result['APIResponse']['Items'][0]['SystemIdentifier'])) {
+        $return['system_identifier'] = $result['APIResponse']['Items'][0]['SystemIdentifier'];
 
-          if (isset($result['APIResponse']['Items'][0]['path_TR1'])) {
-            foreach (['Width', 'Height'] as $key) {
-              if (isset($result['APIResponse']['Items'][0]['path_TR1'][$key])) {
-                $return[strtolower($key)] = $result['APIResponse']['Items'][0]['path_TR1'][$key];
-              }
+        if (isset($result['APIResponse']['Items'][0]['path_TR1'])) {
+          foreach (['Width', 'Height'] as $key) {
+            if (isset($result['APIResponse']['Items'][0]['path_TR1'][$key])) {
+              $return[strtolower($key)] = $result['APIResponse']['Items'][0]['path_TR1'][$key];
             }
           }
         }
