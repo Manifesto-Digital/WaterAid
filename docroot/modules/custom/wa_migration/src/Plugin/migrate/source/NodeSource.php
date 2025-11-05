950681e569b96e9013f89055724901671cf627a1
Paul Smith <paul.smith@manifesto.co.uk>
2025-11-05 16:17:16 +0000

    feat (WA-419): Added biography item migration.

--- docroot/modules/custom/wa_migration/src/Plugin/migrate/source/NodeSource.php
+++ docroot/modules/custom/wa_migration/src/Plugin/migrate/source/NodeSource.php
@@ -105,6 +105,7 @@ final class NodeSource extends SqlBase {
                   'field_enhanced_carousel_items',
                   'field_tab_items',
                   'field_quotes_quote',
+                  'field_biography_item',
                 ] as $table) {
                   if ($sub_paragraphs = $this->select('paragraph__' . $table, 'p')
                     ->fields('p')
