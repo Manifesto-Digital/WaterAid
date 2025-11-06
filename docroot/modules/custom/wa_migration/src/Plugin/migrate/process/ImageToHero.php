64a33791d4b4defbc1c781cf67131d8eca00a47c
Paul Smith <paul.smith@manifesto.co.uk>
2025-11-06 11:50:07 +0000

    feat (WA-419): Remove unrequired fields from site and migrations, and add plugin to create heros for an image.

---
+++ docroot/modules/custom/wa_migration/src/Plugin/migrate/process/ImageToHero.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\wa_migration\Plugin\migrate\process;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Drupal\paragraphs\ParagraphInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides an image_to_hero plugin.
+ *
+ * Usage:
+ *
+ * @code
+ * process:
+ *   bar:
+ *     plugin: image_to_hero
+ *     source: foo
+ *     hero_type: <type of hero paragraph to create>
+ * @endcode
+ *
+ * @MigrateProcessPlugin(id = "image_to_hero")
+ */
+final class ImageToHero extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Constructs the plugin instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property): ?ParagraphInterface {
+    if (!empty($value[0][0])) {
+      if ($media = $this->entityTypeManager->getStorage('media')->load($value[0][0])) {
+
+        /** @var \Drupal\paragraphs\ParagraphInterface $paragraph */
+        $paragraph = $this->entityTypeManager->getStorage('paragraph')->create([
+          'type' => $this->configuration['hero_type'],
+        ]);
+        $paragraph->set('field_image', $media);
+
+        if (!empty($value[1])) {
+          $paragraph->set('field_authors', $value[1]);
+        }
+
+        $paragraph->enforceIsNew();
+
+        return $paragraph;
+      }
+    }
+
+    return NULL;
+  }
+
+}
