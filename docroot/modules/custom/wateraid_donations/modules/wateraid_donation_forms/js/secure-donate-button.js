0d0c436aee05b6d9ae5e0744822a24ee8f3dda91
Endre Soo <endre.soo@tpximpact.com>
2025-11-11 22:09:17 +0000

    [WA-427] Donation widget can be Only monthly or only one off

--- docroot/modules/custom/wateraid_donations/modules/wateraid_donation_forms/js/secure-donate-button.js
+++ docroot/modules/custom/wateraid_donations/modules/wateraid_donation_forms/js/secure-donate-button.js
@@ -8,13 +8,12 @@
 
   Drupal.behaviors.secureDonateButton = {
     attach(context) {
-      console.log('secure-donate-button.js loaded');
       context.querySelectorAll('.input--submit--secure').forEach((element) => {
         // If this is the payment step, the normal submit button may be hidden
         // in favour of the payment provider specific buttons. If the button
         // is hidden in this way, we should ensure our wrapper and secure image
         // are also hidden.
-        const formActions = element?.closest('form')?.querySelector('#edit-actions');
+        const formActions = element?.closest('form')?.querySelector('[data-drupal-selector="edit-actions"]');
         if (formActions) {
           formActions.style.display = 'none';
         }
