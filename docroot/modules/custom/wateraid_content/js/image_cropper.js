15a6cf4daf235330d37b77031dbbe5b4cc09bf30
Endre Soo <endre.soo@tpximpact.com>
2026-01-13 09:50:27 +0000

    [WA-790] Add csutom crop initial code

---
+++ docroot/modules/custom/wateraid_content/js/image_cropper.js
@@ -0,0 +1,15 @@
+const Cropper = require('cropperjs');
+
+(function (Drupal) {
+  'use strict';
+
+  Drupal.behaviors.wateraidContentImageFocalPoint = {
+    attach: function (context, settings) {
+      console.log('Focal Point JS loaded');
+      // create image with src
+      const elem
+      // https://dam-cdn.wi0.orangelogic.com/AssetLink/8w21j2ih7s4e5q5e504s15n766rb8tbw-1440x480,CentreCrop.jpg
+    }
+  };
+
+})(Drupal);
