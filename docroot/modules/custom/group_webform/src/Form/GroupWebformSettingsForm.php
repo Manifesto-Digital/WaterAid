b5140c1e2d9f0711a3d8c84934fd61983da813d2
Paul Smith <paul.smith@manifesto.co.uk>
2026-02-20 10:36:33 +0000

    feat (WA-254): Refactor queue services so they can complete in time, and add new queue to populate email notices database table.

--- docroot/modules/custom/group_webform/src/Form/GroupWebformSettingsForm.php
+++ docroot/modules/custom/group_webform/src/Form/GroupWebformSettingsForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\group_webform\Form;
 
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\group_webform\GroupWebformService;
@@ -62,6 +63,30 @@ class GroupWebformSettingsForm extends ConfigFormBase {
       '#description' => $this->t("By default the Group Webform Select Field only shows webforms with Group Relationships. Check to show all user-accessible webforms in the Select Field. Additonal webforms will only appear for users with 'Administer webform' permission."),
       '#default_value' => $config->get('show_all_webforms_in_gwselect'),
     ];
+
+    $result = 'placeholder';
+    $start = 0;
+
+    $future = strtotime('now +3 months');
+
+    while (!empty($result)) {
+      $query = \Drupal::database()->select('media__field_dam_expiry_date', 'e')
+        ->condition('field_dam_expiry_date_value', $future, '<');
+      $query->leftJoin('entity_usage', 'u', 'u.target_id = e.entity_id AND u.target_type = :type', [
+        ':type' => 'media',
+      ]);
+      $query->leftJoin('entity_usage', 'pu', 'pu.target_id = u.source_id AND pu.target_type = u.source_type');
+      $query->fields('e', ['entity_id', 'field_dam_expiry_date_value']);
+      $query->fields('u', ['source_id', 'source_type']);
+      $query->range($start, 100);
+      $result = $query->execute()->fetchAll();
+
+      foreach ($result as $item) {
+        \Drupal::queue('wa_orange_dam_usage_processor')->createItem($item);
+      }
+
+      $start += 100;
+    }
 
     return $form;
   }
